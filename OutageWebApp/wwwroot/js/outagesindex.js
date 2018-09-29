1da5e33e548859c7a29c4362a7d760c2b14e31cb
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-09-29 12:58:17 +0530

    Fixes #3

--- OutageWebApp/wwwroot/js/outagesindex.js
+++ OutageWebApp/wwwroot/js/outagesindex.js
@@ -14,7 +14,8 @@
         fixedColumns: true,
         lengthMenu: [[10, 25, 50, 100, 500, 1000, -1], [10, 25, 50, 100, 500, 1000, "All"]],
         buttons: ['copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5'],
-        pageLength: 50
+        pageLength: 50,
+        "order": [[3, "desc"]]
     });
 
     // Filter event handler
